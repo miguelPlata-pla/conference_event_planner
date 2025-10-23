618135c01adae459dabf7e5e8606480ece8d297c
Angel <angelplatasilver@gmail.com>
2025-10-23 19:27:59 -0400

    avance

--- src/store.js
+++ src/store.js
@@ -1,9 +1,10 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
-
+import avReduceer from './avSlice';
 export default configureStore({
   reducer: {
     venue: venueReducer,
+    av: avReduceer,
   },
 });
